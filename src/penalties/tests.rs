5bdcaf6e058d5f12197d506cc5098847074e5428
PAB <pierreantoine.bannier@gmail.com>
2021-12-27 19:03:44 +0100

    Add datafits (#2)
    * fix name for L1
    
    * WIP: added quadratic datafit
    
    * WIP: added tests
    
    * fix small mistakes
    
    * fix datafit mod
    
    * all tests are passing
    
    * fix macro
    
    * implemented Default trait for Quadratic datafit

--- src/penalties/tests.rs
+++ src/penalties/tests.rs
@@ -7,7 +7,7 @@ use ndarray::Array1;
 #[test]
 fn test_value_l1() {
     let a = Array1::from_shape_vec(5, vec![3.4, 2.1, -2.3, -0.3, 4.5]).unwrap();
-    let pen = L1Penalty { alpha: 3.2 };
+    let pen = L1 { alpha: 3.2 };
     let val = pen.value(a.view());
     assert_eq!(val, 40.32);
 }
@@ -17,7 +17,7 @@ fn test_prox_l1() {
     let a = 0.3;
     let b = 12.4;
     let c = -49.2;
-    let pen = L1Penalty { alpha: 2. };
+    let pen = L1 { alpha: 2. };
     let soft_a = pen.prox_op(a, 1. / 0.5, 0);
     let soft_b = pen.prox_op(b, 1. / 0.5, 0);
     let soft_c = pen.prox_op(c, 1. / 0.5, 0);
@@ -31,7 +31,7 @@ fn test_subdiff_dist_l1() {
     let w = Array1::from_shape_vec(3, vec![-3.3, 0.1, 3.2]).unwrap();
     let grad = Array1::from_shape_vec(3, vec![0.4, 3.2, -3.4]).unwrap();
     let ws = Array1::from_shape_vec(3, (0..3).collect()).unwrap();
-    let pen = L1Penalty { alpha: 1. };
+    let pen = L1 { alpha: 1. };
     let subdiff_dist = pen.subdiff_distance(w.view(), grad.view(), ws.view());
     let res = Array1::from_shape_vec(3, vec![0.6, 4.2, 2.4]).unwrap();
     assert_eq!(subdiff_dist, res);
