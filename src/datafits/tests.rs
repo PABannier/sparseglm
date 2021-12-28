1835616cb371a2934eab606e08418afcd886ce69
Pierre-Antoine Bannier <pierreantoine.bannier@gmail.com>
2021-12-28 11:02:48 +0100

    streamlined and refactored assert_array_all_close macro

--- src/datafits/tests.rs
+++ src/datafits/tests.rs
@@ -1,24 +1,19 @@
 extern crate ndarray;
+extern crate num;
 
 use crate::datafits::*;
 
 use ndarray::{Array1, Array2};
+use num::Float;
+use std::fmt::Display;
 
-macro_rules! assert_delta {
-    ($x:expr, $y:expr, $d:expr) => {
-        if !($x - $y < $d || $y - $x < $d) {
-            panic!();
+fn assert_array_all_close<T: Float + Display>(x: ArrayView1<T>, y: ArrayView1<T>, delta: T) {
+    assert_eq!(x.len(), y.len());
+    for i in 0..x.len() {
+        if !(T::abs(x[i] - y[i]) < delta) {
+            panic!("x: {}, y: {} ; with precision level {}", x[i], y[i], delta);
         }
-    };
-}
-
-macro_rules! assert_delta_arr {
-    ($x:expr, $y: expr, $d: expr) => {
-        assert_eq!($x.len(), $y.len());
-        for i in 0..$x.len() {
-            assert_delta!($x[i], $y[i], $d);
-        }
-    };
+    }
 }
 
 #[test]
@@ -29,8 +24,8 @@ fn test_initialization_quadratic() {
     df.initialize(X.view(), y.view());
     let Xty = Array1::from_shape_vec(3, vec![-4.42, -9.66, -7.4]).unwrap();
     let lipschitz = Array1::from_shape_vec(3, vec![11.56, 2.925, 2.665]).unwrap();
-    assert_delta_arr!(df.Xty, Xty, 1e-9);
-    assert_delta_arr!(df.lipschitz, lipschitz, 1e-9);
+    assert_array_all_close(Xty.view(), df.get_Xty().view(), 1e-8);
+    assert_array_all_close(lipschitz.view(), df.get_lipschitz().view(), 1e-8);
 }
 
 #[test]
