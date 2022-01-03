e5f40894389ee99322f0ac5fd3428578bbcb2947
Pierre-Antoine Bannier <pierreantoine.bannier@gmail.com>
2022-01-03 17:19:12 +0100

    added sparse methods to quadratic datafit

--- src/datafits/mod.rs
+++ src/datafits/mod.rs
@@ -4,11 +4,14 @@ extern crate num;
 use ndarray::{s, Array1, ArrayView1, ArrayView2, Axis};
 use num::Float;
 
+use crate::sparse::CSRArray;
+
 #[cfg(test)]
 mod tests;
 
 pub trait Datafit<T: Float> {
     fn initialize(&mut self, X: ArrayView2<T>, y: ArrayView1<T>);
+    fn initialize_sparse(&mut self, X: &mut CSRArray<T>, y: ArrayView1<T>);
     fn value(&self, y: ArrayView1<T>, w: ArrayView1<T>, Xw: ArrayView1<T>) -> T;
     fn gradient_scalar(
         &self,
@@ -18,7 +21,19 @@ pub trait Datafit<T: Float> {
         Xw: ArrayView1<T>,
         j: usize,
     ) -> T;
-
+    fn gradient_scalar_sparse(
+        &self,
+        X: &mut CSRArray<T>,
+        y: ArrayView1<T>,
+        Xw: ArrayView1<T>,
+        j: usize,
+    ) -> T;
+    fn full_grad_sparse(
+        &self,
+        X: &mut CSRArray<T>,
+        y: ArrayView1<T>,
+        Xw: ArrayView1<T>,
+    ) -> Array1<T>;
     fn get_lipschitz(&self) -> ArrayView1<T>;
     fn get_Xty(&self) -> ArrayView1<T>;
 }
@@ -48,6 +63,22 @@ impl<'a, T: 'static + Float> Datafit<T> for Quadratic<T> {
         let lc = X.map_axis(Axis(0), |Xj| Xj.dot(&Xj) / n_samples);
         self.lipschitz = lc;
     }
+    /// Initializes the datafit by pre-computing useful quantites with sparse matrices
+    fn initialize_sparse(&mut self, X: &mut CSRArray<T>, y: ArrayView1<T>) {
+        let n_features = X.indptr.len() - 1;
+        self.Xty = Array1::<T>::zeros(n_features);
+        self.lipschitz = Array1::<T>::zeros(n_features);
+        for j in 0..n_features {
+            let nrm2 = 0.;
+            let xty = 0;
+            for idx in X.indptr[j]..X.indptr[j + 1] {
+                nrm2 += X.data[idx].pow(2);
+                xty += X.data[idx] * y[X.indices[idx]];
+            }
+            self.lipschitz[j] = nrm2 / y.len();
+            self.Xty[j] = xty;
+        }
+    }
     /// Computes the value of the datafit
     fn value(&self, y: ArrayView1<T>, _w: ArrayView1<T>, Xw: ArrayView1<T>) -> T {
         let r = &y - &Xw;
@@ -55,7 +86,7 @@ impl<'a, T: 'static + Float> Datafit<T> for Quadratic<T> {
         let val = r.dot(&r) / denom;
         val
     }
-    /// Computes the value of the gradient at some point w
+    /// Computes the value of the gradient at some point w for coordinate j
     fn gradient_scalar(
         &self,
         X: ArrayView2<T>,
@@ -67,6 +98,39 @@ impl<'a, T: 'static + Float> Datafit<T> for Quadratic<T> {
         let n_samples = T::from(Xw.len()).unwrap();
         let Xj: ArrayView1<T> = X.slice(s![.., j]);
         (Xj.dot(&Xw) - self.Xty[j]) / n_samples
+    }
+    /// Computes the value of the gradient at some point w for coordinate j using sparse matrices
+    fn gradient_scalar_sparse(
+        &self,
+        X: &CSRArray<T>,
+        y: ArrayView1<T>,
+        Xw: ArrayView1<T>,
+        j: usize,
+    ) -> T {
+        let XjTXw = 0.;
+        for i in X.indptr[j]..X.indptr[j + 1] {
+            XjTXw += X.data[i] * Xw[X.indices[i]];
+        }
+        return (XjTXw - self.Xty[j]) / Xw.len();
+    }
+    /// Computes the gradient at some point w using sparse matrices
+    fn full_grad_sparse(
+        &self,
+        X: &mut CSRArray<T>,
+        y: ArrayView1<T>,
+        Xw: ArrayView1<T>,
+    ) -> Array1<T> {
+        let n_features = X.indptr.len() - 1;
+        let n_samples = y.len();
+        let mut grad = Array1::<T>::zeros(n_features);
+        for j in 0..n_features {
+            let XjTXw = 0.;
+            for i in X.indptr[j]..X.indptr[j + 1] {
+                XjTXw += X.data[i] * Xw[X.indices[i]];
+            }
+            grad[j] = (XjTXw - self.Xty[j]) / n_samples;
+        }
+        grad
     }
     // Getter for Lipschitz
     fn get_lipschitz(&self) -> ArrayView1<T> {
