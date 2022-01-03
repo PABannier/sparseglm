ee315162c0fb305ed0f5ad0d3d6e98d8157a9906
Pierre-Antoine Bannier <pierreantoine.bannier@gmail.com>
2022-01-03 17:23:21 +0100

    a bit of cleaning

--- src/datafits/mod.rs
+++ src/datafits/mod.rs
@@ -13,27 +13,18 @@ pub trait Datafit<T: Float> {
     fn initialize(&mut self, X: ArrayView2<T>, y: ArrayView1<T>);
     fn initialize_sparse(&mut self, X: &mut CSRArray<T>, y: ArrayView1<T>);
     fn value(&self, y: ArrayView1<T>, w: ArrayView1<T>, Xw: ArrayView1<T>) -> T;
+    #[rustfmt::skip]
     fn gradient_scalar(
-        &self,
-        X: ArrayView2<T>,
-        y: ArrayView1<T>,
-        w: ArrayView1<T>,
-        Xw: ArrayView1<T>,
-        j: usize,
-    ) -> T;
+        &self, X: ArrayView2<T>, y: ArrayView1<T>, w: ArrayView1<T>,
+        Xw: ArrayView1<T>, j: usize) -> T;
+    #[rustfmt::skip]
     fn gradient_scalar_sparse(
-        &self,
-        X: &mut CSRArray<T>,
-        y: ArrayView1<T>,
-        Xw: ArrayView1<T>,
-        j: usize,
-    ) -> T;
+        &self, X: &mut CSRArray<T>, y: ArrayView1<T>, Xw: ArrayView1<T>,
+        j: usize) -> T;
+    #[rustfmt::skip]
     fn full_grad_sparse(
-        &self,
-        X: &mut CSRArray<T>,
-        y: ArrayView1<T>,
-        Xw: ArrayView1<T>,
-    ) -> Array1<T>;
+        &self, X: &mut CSRArray<T>, y: ArrayView1<T>, Xw: ArrayView1<T>
+        ) -> Array1<T>;
     fn get_lipschitz(&self) -> ArrayView1<T>;
     fn get_Xty(&self) -> ArrayView1<T>;
 }
@@ -87,25 +78,18 @@ impl<'a, T: 'static + Float> Datafit<T> for Quadratic<T> {
         val
     }
     /// Computes the value of the gradient at some point w for coordinate j
+    #[rustfmt::skip]
     fn gradient_scalar(
-        &self,
-        X: ArrayView2<T>,
-        _y: ArrayView1<T>,
-        _w: ArrayView1<T>,
-        Xw: ArrayView1<T>,
-        j: usize,
-    ) -> T {
+        &self, X: ArrayView2<T>, _y: ArrayView1<T>, _w: ArrayView1<T>,
+        Xw: ArrayView1<T>, j: usize) -> T {
         let n_samples = T::from(Xw.len()).unwrap();
         let Xj: ArrayView1<T> = X.slice(s![.., j]);
         (Xj.dot(&Xw) - self.Xty[j]) / n_samples
     }
     /// Computes the value of the gradient at some point w for coordinate j using sparse matrices
+    #[rustfmt::skip]
     fn gradient_scalar_sparse(
-        &self,
-        X: &CSRArray<T>,
-        y: ArrayView1<T>,
-        Xw: ArrayView1<T>,
-        j: usize,
+        &self, X: &CSRArray<T>, y: ArrayView1<T>, Xw: ArrayView1<T>, j: usize
     ) -> T {
         let XjTXw = 0.;
         for i in X.indptr[j]..X.indptr[j + 1] {
@@ -114,11 +98,9 @@ impl<'a, T: 'static + Float> Datafit<T> for Quadratic<T> {
         return (XjTXw - self.Xty[j]) / Xw.len();
     }
     /// Computes the gradient at some point w using sparse matrices
+    #[rustfmt::skip]
     fn full_grad_sparse(
-        &self,
-        X: &mut CSRArray<T>,
-        y: ArrayView1<T>,
-        Xw: ArrayView1<T>,
+        &self, X: &mut CSRArray<T>, y: ArrayView1<T>, Xw: ArrayView1<T>,
     ) -> Array1<T> {
         let n_features = X.indptr.len() - 1;
         let n_samples = y.len();
