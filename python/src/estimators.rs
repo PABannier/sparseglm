81a2cae7300404dd782aba7457f0e7ec6c89f78b
Pierre-Antoine Bannier <pierreantoine.bannier@gmail.com>
2022-01-04 18:29:55 +0100

    added method definition

--- python/src/estimators.rs
+++ python/src/estimators.rs
@@ -1,7 +1,7 @@
+use numpy::ndarray::{PyArray1, PyArray2};
 use pyo3::prelude::*;
-use pyo3::types::PyList;
 
-use RustyLasso::estimators::*;
+use rustylasso::estimators::*;
 
 #[pyclass(module = "RustyLasso.estimators")]
 pub struct BaseEstimator {}
@@ -25,7 +25,7 @@ pub struct Lasso {
 #[pymethods]
 impl Lasso {
     #[new]
-    fn new(alpha: Number) -> PyResult<(Self, BaseEstimator)> {
+    fn new(alpha: &PyFloat) -> PyResult<(Self, BaseEstimator)> {
         let estimator = RustyLasso::estimators::Lasso::new(alpha);
 
         Ok((Lasso { inner: estimator }, BaseEstimator::new()))
@@ -37,9 +37,41 @@ impl Lasso {
     ///
     /// Parameters
     /// ----------
-    /// X:
+    /// X:  PyArray2
     ///    The design matrix
-    /// y:
+    /// y:  PyArray1
     ///    Measurement vector
-    fn fit<'py>(&self, py: Python<'py>, X: ArrayView2<T>, y: ArrayView1<T>) -> PyResult {}
+    fn fit<'py>(&self, py: Python<'py>, X: PyArray2<f64>, y: PyArray1<f64>) -> PyResult<PyArray1> {
+        Ok(self.inner.fit(X, y))
+    }
+
+    /// fit_sparse(self, X, y)
+    ///
+    /// Fits the estimator to a sparse design matrix
+    ///
+    /// Parameters
+    /// ----------
+    /// X: CSRArray
+    ///     The sparse design matrix
+    /// y: PyArray1
+    ///     Measurement vector
+    fn fit_sparse<'py>(
+        &self,
+        py: Python<'py>,
+        X: PyCSRArray<f64>,
+        y: PyArray1<f64>,
+    ) -> PyResult<PyArray1> {
+        Ok(self.inner.fit_sparse(X, y))
+    }
+
+    /// get_params(self)
+    /// Get parameters for this estimator.
+    ///
+    /// Returns
+    /// -------
+    /// params : mapping of string to any
+    ///          Parameter names mapped to their values.
+    fn get_params(&self) -> PyResult<SolverParams> {
+        Ok(self.inner.params.clone())
+    }
 }
