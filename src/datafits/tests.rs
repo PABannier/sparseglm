420dbab0d7a0db08be584b9f68463a82a0a717c3
Pierre-Antoine Bannier <pierreantoine.bannier@gmail.com>
2022-01-04 14:13:41 +0100

    WIP: tests for quadratic sparse init are not passing

--- src/datafits/tests.rs
+++ src/datafits/tests.rs
@@ -5,6 +5,7 @@ use ndarray::{Array1, Array2};
 
 use crate::datafits::*;
 use crate::helpers::test_helpers::*;
+use crate::sparse::*;
 
 #[test]
 fn test_initialization_quadratic() {
@@ -16,6 +17,29 @@ fn test_initialization_quadratic() {
     let lipschitz = Array1::from_shape_vec(3, vec![11.56, 2.925, 2.665]).unwrap();
     assert_array_all_close(Xty.view(), df.get_Xty().view(), 1e-8);
     assert_array_all_close(lipschitz.view(), df.get_lipschitz().view(), 1e-8);
+}
+
+#[test]
+fn test_initialization_sparse_quadratic() {
+    let indptr = vec![0, 2, 3, 6];
+    let indices = vec![0, 2, 2, 0, 1, 2];
+    let data = vec![1., 2., 3., 4., 5., 6.];
+    let X_sparse = CSRArray::new(data, indices, indptr);
+    let X = Array2::from_shape_vec((3, 3), vec![1., 0., 2., 0., 0., 3., 4., 5., 6.]).unwrap();
+    let y = Array1::from_shape_vec(3, vec![1., 3., 2.]).unwrap();
+
+    let mut df_sparse = Quadratic::default();
+    df_sparse.initialize_sparse(&X_sparse, y.view());
+
+    let mut df = Quadratic::default();
+    df.initialize(X.view(), y.view());
+
+    assert_array_all_close(df.get_Xty().view(), df_sparse.get_Xty().view(), 1e-8);
+    assert_array_all_close(
+        df.get_lipschitz().view(),
+        df_sparse.get_lipschitz().view(),
+        1e-8,
+    );
 }
 
 #[test]
@@ -39,4 +63,28 @@ fn test_gradient_quadratic() {
     df.initialize(X.view(), y.view());
     let grad = df.gradient_scalar(X.view(), y.view(), w.view(), Xw.view(), 1);
     assert_eq!(grad, 9.583749999999998);
+}
+
+#[test]
+fn test_gradient_sparse_quadratic() {
+    let indptr = vec![0, 2, 3, 6];
+    let indices = vec![0, 2, 2, 0, 1, 2];
+    let data = vec![1., 2., 3., 4., 5., 6.];
+    let X_sparse = CSRArray::new(data, indices, indptr);
+
+    let X = Array2::from_shape_vec((3, 3), vec![1., 0., 2., 0., 0., 3., 4., 5., 6.]).unwrap();
+
+    let y = Array1::from_shape_vec(3, vec![1., 3., 2.]).unwrap();
+    let w = Array1::from_shape_vec(3, vec![-3.2, -0.25, 3.3]).unwrap();
+
+    let Xw = X.dot(&w);
+
+    let mut df = Quadratic::default();
+    let mut df_sparse = Quadratic::default();
+    df.initialize(X.view(), y.view());
+    df_sparse.initialize_sparse(&X_sparse, y.view());
+
+    let grad = df.gradient_scalar(X.view(), y.view(), w.view(), Xw.view(), 1);
+    let grad_sparse = df_sparse.gradient_scalar_sparse(&X_sparse, y.view(), Xw.view(), 1);
+    assert_eq!(grad, grad_sparse);
 }
