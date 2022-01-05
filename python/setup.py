f6bb6bc9a175e966eb174112103b96ef64a719ca
Pierre-Antoine Bannier <pierreantoine.bannier@gmail.com>
2022-01-05 15:57:20 +0100

    setting up python packages

---
+++ python/setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+from setuptools_rust import RustExtension
+
+
+with open("./requirements.txt", "rt") as infile:
+    install_requires = infile.read().splitlines()
+
+setup(
+    version="0.1.0",
+    rust_extensions=[
+        RustExtension(
+            "rustylasso._lib",
+            rustc_flags=["--cfg=Py_3"],
+            features=["numpy/python3"],
+            args=["--no-default-features"],
+        )
+    ],
+    python_requires=">=3.6",
+    install_requires=install_requires,
+    packages=find_packages(),
+)
