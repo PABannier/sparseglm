8e904561060267e5f933677c5566c45f1dda746d
PAB <pierreantoine.bannier@gmail.com>
2022-01-05 17:45:45 +0100

    Create Python bindings (#9)
    * created python folder
    
    * clean
    
    * created python lib module
    
    * added estimators module to pyo3 crate
    
    * added maturin
    
    * numpy dependencies
    
    * added method definition
    
    * better dependencies
    
    * maturin now compiles
    
    * improved linting
    
    * cln
    
    * fix PyFloat issues
    
    * lasso is compiling
    
    * added Lasso estimator class in Python
    
    * added params to lassowrapper
    
    * fix tests
    
    * setting up python packages
    
    * various fixes
    
    * added tests for scikit learn
    
    * added flake for python code
    
    * build is successful
    
    * git ignore'
    
    * fix linting issues
    
    * added pytest CI
    
    * fix pytest
    
    * fix
    
    * added validation for matrices
    
    * enh pytest ci

---
+++ python/rustylasso/estimators.py
@@ -0,0 +1,84 @@
+from . import _lib
+
+import scipy.sparse as sp
+from sklearn.base import BaseEstimator
+from sklearn.utils.validation import check_X_y
+
+
+__all__ = ["Lasso"]
+
+
+class Lasso(BaseEstimator):
+    """Solves a L1-regularized least square linear regression.
+
+    The solver uses Anderson acceleration combined with a working set strategy
+    for faster convergence.
+
+    Parameters
+    ----------
+
+    TODO
+
+    Examples
+    --------
+    >>> from rustylasso.estimators import Lasso
+    >>> clf = Lasso(alpha)
+    >>> clf.fit(X, y)
+
+    """
+
+    def __init__(self, alpha, max_iter=50, max_epochs=1000, tol=1e-9, p0=10,
+                 use_accel=True, K=5, verbose=True):
+        self.alpha = alpha
+        self.max_iter = max_iter
+        self.max_epochs = max_epochs
+        self.tol = tol
+        self.p0 = p0
+        self.use_accel = use_accel
+        self.K = K
+        self.verbose = verbose
+
+        self.coef_ = None
+
+    def _validate_params(self):
+        def _check_types(var, type, var_name):
+            if not isinstance(var, type) or var < 0:
+                raise ValueError("{}={} must be a positive {}."
+                                 .format(var_name, self.alpha, type))
+
+        _check_types(self.alpha, float, "alpha")
+        _check_types(self.max_iter, int, "max_iter")
+        _check_types(self.max_epochs, int, "max_epochs")
+        _check_types(self.tol, float, "tol")
+        _check_types(self.p0, int, "p0")
+        _check_types(self.K, int, "K")
+
+    def fit(self, X, y):
+        """Solves the L1-regularized linear regression to the data (X, y).
+
+        Parameters
+        ----------
+        X : array-like, shape (n_samples, n_features)
+            Design matrix.
+
+        y : array-like, shape (n_samples)
+            Measurements.
+        """
+        self._validate_params()
+        X, y = check_X_y(X, y, accept_sparse='csr', order="C")
+        self._inner = _lib.LassoWrapper(
+            alpha=self.alpha, max_iter=self.max_iter, p0=self.p0, K=self.K,
+            max_epochs=self.max_epochs, tol=self.tol, verbose=self.verbose)
+
+        if sp.issparse(X):
+            raise NotImplementedError("Sparse matrices not yet implemented.")
+        else:
+            coefs = self._inner.fit(X, y)
+        self.coef_ = coefs.T
+
+        return self
+
+    def predict(self, X):
+        """Predits the test set."""
+        # TODO: validation of X
+        return X @ self.coef_.T
---
+++ python/src/estimators.rs
@@ -0,0 +1,66 @@
+use numpy::{PyArray, PyArray1, PyArray2};
+use pyo3::prelude::*;
+use rustylasso;
+use rustylasso::estimators::Estimator;
+
+#[pyclass(module = "rustylasso.estimators")]
+pub struct BaseEstimator {}
+
+#[pymethods]
+impl BaseEstimator {
+    #[new]
+    pub fn new() -> Self {
+        BaseEstimator {}
+    }
+}
+
+/// __init__(self)
+///
+/// Lasso estimator
+#[pyclass(extends=BaseEstimator, module="rustylasso.estimators")]
+pub struct LassoWrapper {
+    inner: rustylasso::estimators::Lasso<f32>,
+}
+
+#[pymethods]
+impl LassoWrapper {
+    #[new]
+    fn new(
+        alpha: f32,
+        max_iter: usize,
+        max_epochs: usize,
+        tol: f32,
+        p0: usize,
+        use_accel: bool,
+        K: usize,
+        verbose: bool,
+    ) -> PyResult<(Self, BaseEstimator)> {
+        let params = rustylasso::estimators::SolverParams::new(
+            max_epochs, max_iter, p0, tol, K, use_accel, verbose,
+        );
+        let estimator = rustylasso::estimators::Lasso::new(alpha, Some(params));
+        Ok((LassoWrapper { inner: estimator }, BaseEstimator::new()))
+    }
+
+    /// fit(self, X, y)
+    ///
+    /// Fits the estimator to the data
+    ///
+    /// Parameters
+    /// ----------
+    /// X: &PyArray2
+    ///    The design matrix
+    /// y: &PyArray1
+    ///    Measurement vector
+    unsafe fn fit<'py>(
+        &mut self,
+        py: Python<'py>,
+        X: &PyArray2<f32>,
+        y: &PyArray1<f32>,
+    ) -> PyResult<&'py PyArray1<f32>> {
+        Ok(PyArray::from_array(
+            py,
+            &self.inner.fit(X.as_array(), y.as_array()),
+        ))
+    }
+}
