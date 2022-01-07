8724a697849a969bb4b9db368f9dccef00738dd3
Pierre-Antoine Bannier <pierreantoine.bannier@gmail.com>
2022-01-07 17:53:41 +0100

    added benchmarks for sparse lasso

--- python/rustylasso/benchmarks/lasso.py
+++ python/rustylasso/benchmarks/lasso.py
@@ -1,5 +1,7 @@
 import time
+
 import numpy as np
+import scipy.sparse as sp
 
 from sklearn.linear_model import Lasso as Lasso_sk
 from rustylasso.estimators import Lasso
@@ -11,7 +13,7 @@ n_features = 10_000
 
 snr = 2
 corr = 0.6
-density = 0.1
+density = 0.5
 
 tol = 1e-12
 
@@ -20,6 +22,8 @@ reg = 0.01
 X, y, _ = make_correlated_data(
     n_samples=n_samples, n_features=n_features, corr=corr, snr=snr,
     density=density, random_state=0)
+
+X_sparse = sp.csc_matrix(X * np.random.normal(0, 1, size=X.shape))
 
 
 def time_estimator(clf, X, y):
@@ -34,10 +38,24 @@ alpha_max = compute_alpha_max(X, y)
 estimator_sk = Lasso_sk(alpha_max * reg, fit_intercept=False, tol=tol)
 estimator_rl = Lasso(alpha_max * reg, tol=tol, verbose=True)
 
+print("Fitting dense matrices...")
+
 coef_sk, duration_sk = time_estimator(estimator_sk, X, y)
 coef_rl, duration_rl = time_estimator(estimator_rl, X, y)
 
 np.testing.assert_allclose(coef_sk, coef_rl, atol=1e-6)
 
-print(f"Scikit-learn :: {duration_sk} s")
-print(f"RustyLasso :: {duration_rl} s")
+print("Fitting sparse matrices...")
+
+coef_sk_sparse, duration_sk_sparse = time_estimator(estimator_sk, X_sparse, y)
+coef_rl_sparse, duration_rl_sparse = time_estimator(estimator_rl, X_sparse, y)
+
+np.testing.assert_allclose(coef_sk_sparse, coef_rl_sparse, atol=1e-6)
+
+print("=" * 5 + " RESULTS " + "=" * 5)
+
+print(f"[DENSE] Scikit-learn :: {duration_sk} s")
+print(f"[DENSE] RustyLasso :: {duration_rl} s")
+print("--" * 5)
+print(f"[SPARSE] Scikit-learn :: {duration_sk_sparse} s")
+print(f"[SPARSE] RustyLasso :: {duration_rl_sparse} s")
