fcbb6f8e61bb398e7d71ed0b94abb6ab46e1b3d7
Pierre-Antoine Bannier <pierreantoine.bannier@gmail.com>
2022-01-08 15:06:23 +0100

    added MTL wrappers

--- python/src/lib.rs
+++ python/src/lib.rs
@@ -1,11 +1,14 @@
 use pyo3::prelude::*;
 
 #[allow(non_snake_case)]
-mod estimators;
+mod lasso;
+mod mtl_lasso;
 
 #[pymodule]
 fn rustylassopy(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_class::<estimators::LassoWrapperF32>()?;
-    m.add_class::<estimators::LassoWrapperF64>()?;
+    m.add_class::<lasso::LassoWrapperF32>()?;
+    m.add_class::<lasso::LassoWrapperF64>()?;
+    m.add_class::<mtl_lasso::MultiTaskLassoWrapperF32>()?;
+    m.add_class::<mtl_lasso::MultiTaskLassoWrapperF64>()?;
     Ok(())
 }
