8a856e4a6999f0f7bfdab032b8cc11119d7e4eb4
Pierre-Antoine Bannier <pierreantoine.bannier@gmail.com>
2022-01-11 13:46:11 +0100

    refactored penalties

--- src/penalties/mod.rs
+++ src/penalties/mod.rs
@@ -1,6 +1,6 @@
 extern crate ndarray;
 
-use ndarray::{Array1, ArrayView1};
+use ndarray::{Array1, ArrayBase, Data, Dimension, Ix1};
 
 use super::Float;
 use crate::helpers::prox::soft_thresholding;
@@ -8,53 +8,67 @@ use crate::helpers::prox::soft_thresholding;
 #[cfg(test)]
 mod tests;
 
-pub trait Penalty<T: Float> {
-    fn value(&self, w: ArrayView1<T>) -> T;
-    fn prox_op(&self, value: T, step_size: T) -> T;
+pub trait Penalty<F, D>
+where
+    F: Float,
+    D: Data<Elem = F>,
+{
+    fn value(&self, w: ArrayBase<D, Ix1>) -> F;
+    fn prox_op(&self, value: F, step_size: F) -> F;
     fn subdiff_distance(
         &self,
-        w: ArrayView1<T>,
-        grad: ArrayView1<T>,
-        ws: ArrayView1<usize>,
-    ) -> (Array1<T>, T);
+        w: ArrayBase<D, Ix1>,
+        grad: ArrayBase<D, Ix1>,
+        ws: ArrayBase<usize, Ix1>,
+    ) -> (ArrayBase<D, Ix1>, F);
 }
 
 /// L1 penalty
 ///
 
-pub struct L1<T> {
-    alpha: T,
+pub struct L1<F>
+where
+    F: Float,
+{
+    alpha: F,
 }
 
-impl<T: Float> L1<T> {
+impl<F> L1<F>
+where
+    F: Float,
+{
     // Constructor
-    pub fn new(alpha: T) -> Self {
+    pub fn new(alpha: F) -> Self {
         L1 { alpha }
     }
 }
 
-impl<T: Float> Penalty<T> for L1<T> {
+impl<F, D> Penalty<F, D> for L1<F>
+where
+    F: Float,
+    D: Data<Elem = F>,
+{
     /// Gets the current value of the penalty
-    fn value(&self, w: ArrayView1<T>) -> T {
+    fn value(&self, w: ArrayBase<D, Ix1>) -> F {
         self.alpha * w.map(|x| (*x).abs()).sum()
     }
     /// Computes the value of the proximal operator
-    fn prox_op(&self, value: T, stepsize: T) -> T {
+    fn prox_op(&self, value: F, stepsize: F) -> F {
         soft_thresholding(value, self.alpha * stepsize)
     }
     /// Computes the distance of the gradient to the subdifferential
     fn subdiff_distance(
         &self,
-        w: ArrayView1<T>,
-        grad: ArrayView1<T>,
-        ws: ArrayView1<usize>,
-    ) -> (Array1<T>, T) {
+        w: ArrayBase<D, Ix1>,
+        grad: ArrayBase<D, Ix1>,
+        ws: ArrayBase<usize, Ix1>,
+    ) -> (ArrayBase<D, Ix1>, F) {
         let ws_size = ws.len();
-        let mut subdiff_dist = Array1::<T>::zeros(ws_size);
-        let mut max_subdiff_dist = T::neg_infinity();
+        let mut subdiff_dist = Array1::<F>::zeros(ws_size);
+        let mut max_subdiff_dist = F::neg_infinity();
         for (idx, &j) in ws.iter().enumerate() {
-            if w[j] == T::zero() {
-                subdiff_dist[idx] = T::max(T::zero(), grad[idx].abs() - self.alpha);
+            if w[j] == F::zero() {
+                subdiff_dist[idx] = F::max(F::zero(), grad[idx].abs() - self.alpha);
             } else {
                 subdiff_dist[idx] = (-grad[idx] - w[j].signum() * self.alpha).abs();
             }
--- src/penalties_multitask/mod.rs
+++ src/penalties_multitask/mod.rs
@@ -1,6 +1,6 @@
 extern crate ndarray;
 
-use ndarray::{s, Array1, ArrayView1, ArrayView2, Axis};
+use ndarray::{s, Array1, ArrayBase, Axis, Data, Ix1, Ix2};
 
 use super::Float;
 use crate::helpers::prox::block_soft_thresholding;
@@ -8,57 +8,71 @@ use crate::helpers::prox::block_soft_thresholding;
 #[cfg(test)]
 mod tests;
 
-pub trait PenaltyMultiTask<T: Float> {
-    fn value(&self, W: ArrayView2<T>) -> T;
-    fn prox_op(&self, value: ArrayView1<T>, stepsize: T) -> Array1<T>;
+pub trait PenaltyMultiTask<F, D>
+where
+    F: Float,
+    D: Data<Elem = F>,
+{
+    fn value(&self, W: ArrayBase<D, Ix2>) -> F;
+    fn prox_op(&self, value: ArrayBase<D, Ix1>, stepsize: F) -> ArrayBase<D, Ix1>;
     fn subdiff_distance(
         &self,
-        W: ArrayView2<T>,
-        grad: ArrayView2<T>,
-        ws: ArrayView1<usize>,
-    ) -> (Array1<T>, T);
+        W: ArrayBase<D, Ix2>,
+        grad: ArrayBase<D, Ix2>,
+        ws: ArrayBase<usize, Ix1>,
+    ) -> (ArrayBase<D, Ix1>, F);
 }
 
 /// L21 penalty
 ///
 
-pub struct L21<T> {
-    alpha: T,
+pub struct L21<F>
+where
+    F: Float,
+{
+    alpha: F,
 }
 
-impl<T: Float> L21<T> {
+impl<F> L21<F>
+where
+    F: Float,
+{
     // Constructor
-    pub fn new(alpha: T) -> Self {
+    pub fn new(alpha: F) -> Self {
         L21 { alpha }
     }
 }
 
-impl<T: 'static + Float> PenaltyMultiTask<T> for L21<T> {
+impl<F, D> PenaltyMultiTask<F, D> for L21<F>
+where
+    F: 'static + Float,
+    D: Data<Elem = F>,
+{
     /// Gets the current value of the penalty
-    fn value(&self, W: ArrayView2<T>) -> T {
+    fn value(&self, W: ArrayBase<D, Ix2>) -> F {
         self.alpha * W.map_axis(Axis(1), |Wj| (Wj.dot(&Wj).sqrt())).sum()
     }
     /// Computes the value of the proximal operator
-    fn prox_op(&self, value: ArrayView1<T>, stepsize: T) -> Array1<T> {
+    fn prox_op(&self, value: ArrayBase<D, Ix1>, stepsize: F) -> ArrayBase<D, Ix1> {
         block_soft_thresholding(value, self.alpha * stepsize)
     }
     /// Computes the distance of the gradient to the subdifferential
     fn subdiff_distance(
         &self,
-        W: ArrayView2<T>,
-        grad: ArrayView2<T>,
-        ws: ArrayView1<usize>,
-    ) -> (Array1<T>, T) {
+        W: ArrayBase<D, Ix2>,
+        grad: ArrayBase<D, Ix2>,
+        ws: ArrayBase<usize, Ix1>,
+    ) -> (ArrayBase<D, Ix1>, F) {
         let ws_size = ws.len();
         let n_tasks = W.shape()[1];
-        let mut subdiff_dist = Array1::zeros(ws_size);
-        let mut max_subdiff_dist = T::neg_infinity();
+        let mut subdiff_dist = Array1::<F>::zeros(ws_size);
+        let mut max_subdiff_dist = F::neg_infinity();
         for (idx, &j) in ws.iter().enumerate() {
-            if W.slice(s![j, ..]).map(|&w| w.abs()).sum() == T::zero() {
+            if W.slice(s![j, ..]).map(|&w| w.abs()).sum() == F::zero() {
                 let norm_grad_j = grad.slice(s![idx, ..]).map(|&x| x * x).sum().sqrt();
-                subdiff_dist[idx] = T::max(T::zero(), norm_grad_j - self.alpha);
+                subdiff_dist[idx] = F::max(F::zero(), norm_grad_j - self.alpha);
             } else {
-                let mut res = Array1::<T>::zeros(n_tasks);
+                let mut res = Array1::<F>::zeros(n_tasks);
                 let norm_W_j = W.slice(s![j, ..]).map(|&wj| wj * wj).sum().sqrt();
                 for t in 0..n_tasks {
                     res[t] = grad[[idx, t]] + self.alpha * W[[j, t]] / norm_W_j;
