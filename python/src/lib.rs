513e57c177c08149777f824762667d56e39551bc
Pierre-Antoine Bannier <pierreantoine.bannier@gmail.com>
2022-01-16 17:07:41 +0100

    working python bindings

--- python/src/lib.rs
+++ python/src/lib.rs
@@ -7,9 +7,7 @@ mod mtl_lasso;
 
 #[pymodule]
 fn rustylassopy(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_class::<lasso::LassoWrapperF32>()?;
-    m.add_class::<lasso::LassoWrapperF64>()?;
-    m.add_class::<mtl_lasso::MultiTaskLassoWrapperF32>()?;
-    m.add_class::<mtl_lasso::MultiTaskLassoWrapperF64>()?;
+    m.add_class::<lasso::LassoWrapper>()?;
+    m.add_class::<mtl_lasso::MultiTaskLassoWrapper>()?;
     Ok(())
 }
