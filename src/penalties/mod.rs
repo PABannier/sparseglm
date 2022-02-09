a407853ddb50a924f0b0520dd703ba9d66fed0a8
PAB <pierreantoine.bannier@gmail.com>
2022-02-10 00:54:52 +0100

    Implementing non-convex penalties (#22)
    * added prox_op and value computation for MCP
    
    * added subdiff_distance computation
    
    * MCP tests passing
    
    * added l05 penalty
    
    * fix l05 penalty + added tests
    
    * added SCAD penalty

--- src/penalties/mod.rs
+++ src/penalties/mod.rs
@@ -3,7 +3,7 @@ extern crate ndarray;
 use ndarray::{Array1, ArrayBase, ArrayView1, Ix1, OwnedRepr};
 
 use super::Float;
-use crate::helpers::prox::soft_thresholding;
+use crate::helpers::prox::{prox_05, soft_thresholding};
 
 #[cfg(test)]
 mod tests;
@@ -78,3 +78,220 @@ where
         (subdiff_dist, max_subdiff_dist)
     }
 }
+
+/// MCP penalty
+///
+
+pub struct MCP<F: Float> {
+    alpha: F,
+    gamma: F,
+}
+
+impl<F: Float> MCP<F> {
+    /// Constructor
+    ///
+    pub fn new(alpha: F, gamma: F) -> Self {
+        MCP { alpha, gamma }
+    }
+}
+
+impl<F: Float> Penalty<F> for MCP<F> {
+    /// Gets the current value of the penalty
+    fn value(&self, w: ArrayView1<F>) -> F {
+        // With x >= 0
+        // pen(x) = alpha * x - x^2 / (2 * gamma) if x =< gamma * alpha
+        //          gamma * alpha 2 / 2           if x > gamma * alpha
+        // value = sum_{j=1}^{n_features} pen(|w_j|)
+        let mut s0 = Array1::from_elem(w.len(), false);
+        for (idx, &wj) in w.iter().enumerate() {
+            if wj.abs() < self.gamma * self.alpha {
+                s0[idx] = true;
+            }
+        }
+        let mut value = Array1::from_elem(w.len(), self.gamma * self.alpha.powi(2) / F::cast(2));
+        for idx in 0..w.len() {
+            if s0[idx] {
+                value[idx] = self.alpha * w[idx].abs() - w[idx].powi(2) / (F::cast(2) * self.gamma);
+            }
+        }
+        value.fold(F::zero(), |sum, &valuej| sum + valuej)
+    }
+
+    /// Proximal operator
+    fn prox_op(&self, value: F, stepsize: F) -> F {
+        let tau = self.alpha * stepsize;
+        let g = self.gamma / stepsize;
+        if value.abs() <= tau {
+            return F::zero();
+        }
+        if value.abs() > g * tau {
+            return value;
+        }
+        return value.signum() * (value.abs() - tau) / (F::one() - F::one() / g);
+    }
+
+    /// Computes the distance of the gradient to the subdifferential
+    fn subdiff_distance(
+        &self,
+        w: ArrayView1<F>,
+        grad: ArrayView1<F>,
+        ws: ArrayView1<usize>,
+    ) -> (ArrayBase<OwnedRepr<F>, Ix1>, F) {
+        let ws_size = ws.len();
+        let mut subdiff_dist = Array1::<F>::zeros(ws_size);
+        let mut max_subdiff_dist = F::neg_infinity();
+        for (idx, &j) in ws.iter().enumerate() {
+            if w[j] == F::zero() {
+                // Distance of -grad to alpha * [-1, 1]
+                subdiff_dist[idx] = F::max(F::zero(), grad[idx].abs() - self.alpha)
+            } else if w[j].abs() < self.alpha * self.gamma {
+                // Distance of -grad_j to (alpha - abs(w[j])/gamma) * sign(w[j])
+                subdiff_dist[idx] =
+                    (grad[idx] + self.alpha * w[j].signum() - w[j] / self.gamma).abs();
+            } else {
+                // Distance of grad to zero
+                subdiff_dist[idx] = grad[idx].abs();
+            }
+
+            if subdiff_dist[idx] > max_subdiff_dist {
+                max_subdiff_dist = subdiff_dist[idx];
+            }
+        }
+        (subdiff_dist, max_subdiff_dist)
+    }
+}
+
+/// L05 penalty
+///
+
+pub struct L05<F: Float> {
+    alpha: F,
+}
+
+impl<F: Float> L05<F> {
+    /// Constructor
+    ///
+    pub fn new(alpha: F) -> Self {
+        L05 { alpha }
+    }
+}
+
+impl<F: Float> Penalty<F> for L05<F> {
+    /// Gets the current value of the penalty
+    fn value(&self, w: ArrayView1<F>) -> F {
+        self.alpha * w.fold(F::zero(), |sum, &wj| sum + wj.abs().sqrt())
+    }
+
+    /// Proximal operator
+    fn prox_op(&self, value: F, stepsize: F) -> F {
+        prox_05(value, stepsize * self.alpha)
+    }
+
+    /// Computes the distance of the gradient to the subdifferential
+    fn subdiff_distance(
+        &self,
+        w: ArrayView1<F>,
+        grad: ArrayView1<F>,
+        ws: ArrayView1<usize>,
+    ) -> (ArrayBase<OwnedRepr<F>, Ix1>, F) {
+        let ws_size = ws.len();
+        let mut subdiff_dist = Array1::<F>::zeros(ws_size);
+        let mut max_subdiff_dist = F::neg_infinity();
+        for (idx, &j) in ws.iter().enumerate() {
+            if w[j] == F::zero() {
+                subdiff_dist[idx] = F::zero();
+            } else {
+                subdiff_dist[idx] = (-grad[idx]
+                    - w[j].signum() * self.alpha / (F::cast(2.) * w[j].abs().sqrt()))
+                .abs();
+            }
+
+            if subdiff_dist[idx] > max_subdiff_dist {
+                max_subdiff_dist = subdiff_dist[idx];
+            }
+        }
+        (subdiff_dist, max_subdiff_dist)
+    }
+}
+
+// SCAD penalty
+// pub struct SCAD<F: Float> {
+//     alpha: F,
+//     gamma: F,
+// }
+
+// impl<F: Float> SCAD<F> {
+//     /// Constructor
+//     ///
+//     pub fn new(alpha: F, gamma: F) -> Self {
+//         SCAD { alpha, gamma }
+//     }
+// }
+
+// impl<F: Float> Penalty<F> for SCAD<F> {
+//     /// Gets the current value of the penalty
+//     fn value(&self, w: ArrayView1<F>) -> F {
+//         // With x >= 0
+//         // pen(x) = alpha * x                                                   if x =< alpha
+//         //          (2 * gamma * alpha * x - x^2 - alpha^2) / (2 * (gamma - 1)) if gamma < x < gamma * lambda
+//         //          alpha^2 * (gamma + 1) / 2                                   if x >= gamma * alpha
+//         let mut s0 = Array1::from_elem(w.len(), false);
+//         let mut s1 = Array1::from_elem(w.len(), false);
+//         for (idx, &wj) in w.iter().enumerate() {
+//             if wj.abs() <= self.alpha {
+//                 s0[idx] = true;
+//             }
+//             if wj.abs() >= self.alpha * self.gamma {
+//                 s1[idx] = true;
+//             }
+//         }
+//         let mut value = Array1::from_elem(
+//             w.len(),
+//             self.alpha * self.alpha * (self.gamma + F::one()) * F::cast(0.5),
+//         );
+//         for idx in 0..w.len() {
+//             if s0[idx] {
+//                 value[idx] = self.alpha * w[idx].abs();
+//             }
+//             if !s0[idx] && !s1[idx] {
+//                 value[idx] = (F::cast(2) * self.alpha * self.gamma * w[idx].abs()
+//                     - w[idx].powi(2)
+//                     - self.alpha.powi(2))
+//                     / (F::cast(2) * (self.gamma - F::one()));
+//             }
+//         }
+//         value.fold(F::zero(), |sum, &valuej| sum + valuej)
+//     }
+
+//     /// Proximal operator
+//     fn prox_op(&self, value: F, stepsize: F) -> F {
+//         let tau = self.alpha * stepsize;
+//         let g = self.gamma / stepsize;
+//         if value.abs() <= F::cast(2) * tau {
+//             return soft_thresholding(value, tau);
+//         } else if value.abs() > g * tau {
+//             return value;
+//         } else {
+//             return (g - F::one()) / (g - F::cast(2))
+//                 * soft_thresholding(value, g * tau / (g - F::one()));
+//         }
+//     }
+
+//     /// Computes the distance of the gradient to the subdifferential
+//     fn subdiff_distance(
+//         &self,
+//         w: ArrayView1<F>,
+//         grad: ArrayView1<F>,
+//         ws: ArrayView1<usize>,
+//     ) -> (ArrayBase<OwnedRepr<F>, Ix1>, F) {
+//         let ws_size = ws.len();
+//         let mut subdiff_dist = Array1::<F>::zeros(ws_size);
+//         let mut max_subdiff_dist = F::neg_infinity();
+//         for (idx, &j) in ws.iter().enumerate() {
+//             if subdiff_dist[idx] > max_subdiff_dist {
+//                 max_subdiff_dist = subdiff_dist[idx];
+//             }
+//         }
+//         (subdiff_dist, max_subdiff_dist)
+//     }
+// }
--- src/penalties/tests.rs
+++ src/penalties/tests.rs
@@ -1,13 +1,13 @@
 extern crate ndarray;
 
-use ndarray::Array1;
+use ndarray::{array, Array1};
 
 use crate::penalties::*;
 
 #[test]
 fn test_value_l1() {
-    let a = Array1::from_shape_vec(5, vec![3.4, 2.1, -2.3, -0.3, 4.5]).unwrap();
-    let pen = L1 { alpha: 3.2 };
+    let a = array![3.4, 2.1, -2.3, -0.3, 4.5];
+    let pen = L1::new(3.2);
     let val = pen.value(a.view());
     assert_eq!(val, 40.32);
 }
@@ -17,7 +17,7 @@ fn test_prox_l1() {
     let a = 0.3;
     let b = 12.4;
     let c = -49.2;
-    let pen = L1 { alpha: 2. };
+    let pen = L1::new(2.);
     let soft_a = pen.prox_op(a, 1. / 0.5);
     let soft_b = pen.prox_op(b, 1. / 0.5);
     let soft_c = pen.prox_op(c, 1. / 0.5);
@@ -28,12 +28,80 @@ fn test_prox_l1() {
 
 #[test]
 fn test_subdiff_dist_l1() {
-    let w = Array1::from_shape_vec(3, vec![-3.3, 0.1, 3.2]).unwrap();
-    let grad = Array1::from_shape_vec(3, vec![0.4, 3.2, -3.4]).unwrap();
+    let w = array![-3.3, 0.1, 3.2];
+    let grad = array![0.4, 3.2, -3.4];
     let ws = Array1::from_shape_vec(3, (0..3).collect()).unwrap();
-    let pen = L1 { alpha: 1. };
+    let pen = L1::new(1.);
     let (subdiff_dist, max_dist) = pen.subdiff_distance(w.view(), grad.view(), ws.view());
-    let res = Array1::from_shape_vec(3, vec![0.6, 4.2, 2.4]).unwrap();
+    let res = array![0.6, 4.2, 2.4];
     assert_eq!(subdiff_dist, res);
     assert_eq!(max_dist, 4.2);
+}
+
+#[test]
+fn test_value_l05() {
+    let a = array![3.4, 2.1, -2.3, -0.3, 4.5];
+    let pen = L05::new(3.2);
+    let val = pen.value(a.view());
+    assert_eq!(val, 23.931726579048444);
+}
+
+#[test]
+fn test_prox_l05() {
+    let a = 0.3;
+    let b = 12.4;
+    let c = -49.2;
+    let pen = L05::new(2.);
+    let soft_a = pen.prox_op(a, 1. / 0.5);
+    let soft_b = pen.prox_op(b, 1. / 0.5);
+    let soft_c = pen.prox_op(c, 1. / 0.5);
+    assert_eq!(soft_a, 0.);
+    assert_eq!(soft_b, 11.818226629068471);
+    assert_eq!(soft_c, -48.91403475649643);
+}
+
+#[test]
+fn test_subdiff_dist_l05() {
+    let w = array![-3.3, 0.1, 3.2];
+    let grad = array![0.4, 3.2, -3.4];
+    let ws = Array1::from_shape_vec(3, (0..3).collect()).unwrap();
+    let pen = L05::new(1.);
+    let (subdiff_dist, max_dist) = pen.subdiff_distance(w.view(), grad.view(), ws.view());
+    let res = array![0.12475905871840987, 4.78113883008419, 3.120491502812526];
+    assert_eq!(subdiff_dist, res);
+    assert_eq!(max_dist, 4.78113883008419);
+}
+
+#[test]
+fn test_value_mcp() {
+    let a = array![3.4, 2.1, -2.3, -0.3, 4.5];
+    let pen = MCP::new(3.2, 3.);
+    let val = pen.value(a.view());
+    assert_eq!(val, 33.38666666666667);
+}
+
+#[test]
+fn test_prox_mcp() {
+    let a = 0.3;
+    let b = 12.4;
+    let c = -49.2;
+    let pen = MCP::new(2., 3.);
+    let pen_a = pen.prox_op(a, 1. / 0.5);
+    let pen_b = pen.prox_op(b, 1. / 0.5);
+    let pen_c = pen.prox_op(c, 1. / 0.5);
+    assert_eq!(pen_a, 0.);
+    assert_eq!(pen_b, 12.4);
+    assert_eq!(pen_c, -49.2);
+}
+
+#[test]
+fn test_subdiff_dist_mcp() {
+    let w = array![-3.2, 0.5, -1.2];
+    let grad = array![0.4, 2.3, -3.2];
+    let ws = Array1::from_shape_vec(3, (0..3).collect()).unwrap();
+    let pen = MCP::new(1., 2.5);
+    let (subdiff_dist, max_dist) = pen.subdiff_distance(w.view(), grad.view(), ws.view());
+    let res = array![0.4, 3.0999999999999996, 3.72];
+    assert_eq!(subdiff_dist, res);
+    assert_eq!(max_dist, 3.72);
 }
