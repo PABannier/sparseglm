ca3ad8ef5f1f4182fd636da993b71e497b04f4d0
Pierre-Antoine Bannier <pierreantoine.bannier@gmail.com>
2022-03-05 13:02:24 +0100

    added Lasso example

---
+++ examples/lasso.rs
@@ -0,0 +1,28 @@
+extern crate rust_sparseglm;
+
+use rust_sparseglm::{
+    datafits::Quadratic,
+    datasets::DatasetBase,
+    estimators::{estimators::Lasso, traits::Fit},
+    helpers::test_helpers::generate_random_data,
+    penalties::L1,
+    solver::{CDSolver, Solver},
+};
+
+fn main() {
+    let (x, y) = generate_random_data(30, 100);
+    let dataset = DatasetBase::from((x, y));
+
+    // Penalty - datafit API
+    let mut datafit = Quadratic::default();
+    let penalty = L1::new(1.);
+    let solver = Solver::default();
+
+    let coefficients = solver.solve(&dataset, &mut datafit, &penalty).unwrap();
+
+    // Estimator API
+    let estimator = Lasso::params().alpha(1.).fit(&dataset).unwrap();
+
+    // Compare coefficients
+    assert_eq!(coefficients, estimator.coefficients());
+}
