3eae5351d347b97bb192456eff8339eea64fef20
Pierre-Antoine Bannier <pierreantoine.bannier@gmail.com>
2022-03-09 22:01:23 +0100

    added elastic net example

---
+++ examples/elastic_net.rs
@@ -0,0 +1,36 @@
+extern crate rust_sparseglm;
+
+use rust_sparseglm::{
+    datafits::Quadratic,
+    datasets::DatasetBase,
+    estimators::{estimators::ElasticNet, traits::Fit},
+    helpers::test_helpers::{assert_array_all_close, generate_random_data},
+    penalties::L1PlusL2,
+    solver::{CDSolver, Solver},
+};
+
+fn main() {
+    let (x, y) = generate_random_data(100, 60);
+    let dataset = DatasetBase::from((x, y));
+
+    let alpha = 0.05;
+    let l1_ratio = 0.7;
+
+    // Penalty - Datafit - Solver API
+    let mut datafit = Quadratic::new();
+    let penalty = L1PlusL2::new(alpha, l1_ratio);
+    let solver = Solver::default();
+
+    println!("#### Fitting with Penalty - Datafit - Solver API...");
+    let coefficients = solver.solve(&dataset, &mut datafit, &penalty).unwrap();
+
+    // Estimator API
+    println!("#### Fitting with the Estimator API...");
+    let estimator = ElasticNet::params()
+        .alpha(alpha)
+        .l1_ratio(l1_ratio)
+        .fit(&dataset)
+        .unwrap();
+
+    assert_array_all_close(coefficients.view(), estimator.coefficients(), 1e-4);
+}
